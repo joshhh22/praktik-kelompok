1934040fce789d6b4d44bd36ffc97d6680101ec7
rifaizPAPASIOAGI <pais.tamvangem@gmail.com>
2025-04-22 08:42:16 +0700

    Tugas

--- register.js
+++ register.js
@@ -1,7 +1,7 @@
 const hargaPerTiket = 75000;
 
         function updateTotalHarga() {
-            const jumlahTiket = document.getElementById('jumlahTiket').value;
+            const jumlahTiket = document.getElementById('jumlah pembelian Tiket').value;
             const totalHarga = jumlahTiket * hargaPerTiket;
             document.getElementById('totalHarga').value = `Rp ${totalHarga.toLocaleString('id-ID')}`;
         }
